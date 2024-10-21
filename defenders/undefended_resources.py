71116f7a4faedafd3260e3d6b25516aa140e344c
jleech-PANW <jleech@paloaltonetworks.com>
2024-10-21 12:55:01 -0400

    Update undefended_resources.py

--- defenders/undefended_resources.py
+++ defenders/undefended_resources.py
@@ -6,7 +6,6 @@ def auth_func():
     login_url = config.get('prismacloud', 'cspm_api_url')
     username = config.get('prismacloud', 'username')
     password = config.get('prismacloud', 'password')
-    print("Logging In...")
     try:
         url = f'{login_url}/login'
         payload = f"{{\n  \"password\": \"{password}\",\n  \"username\": \"{username}\"\n}}"
@@ -17,7 +16,7 @@ def auth_func():
         response = requests.request("POST", url, headers=headers, data=payload)
         response_json = response.json()
         if 'token' in response_json:
-            print("Login Successful")
+            print("Retrieved Token")
             return response_json['token']
         else:
             print("Failed to get authentication token")
@@ -25,50 +24,99 @@ def auth_func():
     except Exception as e:
         print(f"Failed to get authentication token: {e}")
         return None    
-token = auth_func()
+
+
 def entity_report():
+  print("Creating Entity Report")
+  token = auth_func()
   config = configparser.ConfigParser()
   config.read('config.ini')
   base_url = config.get('prismacloud', 'cwp_api_url')
   headers = {
       'Content-Type': 'application/json',
-      'x-redlock-auth': f'{token}'
+      'x-redlock-auth': token
   }
-  payload = {}
-  url = f'{base_url}/api/v33.01/cloud/discovery/entities?defended=false'
+  payload = {}  
+  file_name = "undefended_resources.xlsx"
+  url = f'{base_url}/api/v33.01/cloud/discovery/entities?defended=false&offset=0'
   response = requests.request("GET", url, headers=headers, data=payload)
+  print(f"Initial response Code: {response.status_code}")
   data = json.loads(response.text)
   wb = openpyxl.Workbook()
   ws = wb.active
   ws.title = "Undefended Resources"
-  headers = ["Name", "ARN", "AccountID", "Region"]
-  ws.append(headers)
+  wb_headers = ["Name", "ARN", "AccountID", "Region"]
+  ws.append(wb_headers)
+  offset = 0
   for item in data:
     ws.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
   
-  file_name = "undefended_resources.xlsx"
-  wb.save(file_name)
+  while data is not None:
+    offset += 50
+    url = f'{base_url}/api/v33.01/cloud/discovery/entities?defended=false&offset={offset}'
+    response = requests.request("GET", url, headers=headers, data=payload)
+    try:
+      data = json.loads(response.text)
+    except json.JSONDecodeError:
+       wb.save(file_name)
+       print("Entity Run Complete")
+       return
+    if data is None:
+      wb.save(file_name)
+      print("Entity Run Complete")
+      return
+    for item in data:
+      ws.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
+    wb.save(file_name)
+    print(f"Saved up to {offset}. Response Code: {response.status_code}")
+    if offset % 1000 == 0: 
+      token = auth_func()
 
 def vm_report():
+  print("Creating VM Report")
+  token = auth_func()
   config = configparser.ConfigParser()
   config.read('config.ini')
   base_url = config.get('prismacloud', 'cwp_api_url')
   headers = {
       'Content-Type': 'application/json',
-      'x-redlock-auth': f'{token}'
+      'x-redlock-auth': token
   }
-  payload = {}
-  url = f'{base_url}/api/v33.01/cloud/discovery/vms?hasDefender=false'
-  response = requests.request("GET", url, headers=headers, data=payload)
-  data = json.loads(response.text)
+  payload = {}  
   file_name = "undefended_resources.xlsx"
+  url = f'{base_url}/api/v33.01/cloud/discovery/vms?offset=0&hasDefender=false'
+  response = requests.request("GET", url, headers=headers, data=payload)
+  print(f"Initial response Code: {response.status_code}")
+  data = json.loads(response.text)
   wb = load_workbook(file_name)
   ws2 = wb.create_sheet(title="VMs")
-  headers = ["Name", "ARN", "AccountID", "Region"]
-  ws2.append(headers)
+  wb_headers = ["Name", "ARN", "AccountID", "Region"]
+  ws2.append(wb_headers)
+  offset = 0
   for item in data:
-     ws2.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
-  wb.save(file_name)
-
+    ws2.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
+  
+  while data is not None:
+    offset += 50
+    url = f'{base_url}/api/v33.01/cloud/discovery/vms?offset={offset}&hasDefender=false'
+    response = requests.request("GET", url, headers=headers, data=payload)
+    try:
+      data = json.loads(response.text)
+    except json.JSONDecodeError:
+       wb.save(file_name)
+       print("VM Run Complete")
+       return
+    if data is None:
+      wb.save(file_name)
+      print("VM Run Complete")
+      return
+    for item in data:
+      ws2.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
+    wb.save(file_name)
+    print(f"Saved up to {offset}. Response Code: {response.status_code}")
+    if offset % 1000 == 0: 
+      token = auth_func()
+  
+  
 entity_report()
 vm_report()
