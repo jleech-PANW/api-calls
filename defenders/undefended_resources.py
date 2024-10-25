8b815735c9eed2a61ef36852c4c7c25367eff2fa
jleech-PANW <jleech@paloaltonetworks.com>
2024-10-25 12:38:05 -0400

    Update undefended_resources.py

--- defenders/undefended_resources.py
+++ defenders/undefended_resources.py
@@ -1,5 +1,9 @@
 import requests, json, configparser, openpyxl
+from datetime import date
 from openpyxl import load_workbook
+
+today = date.today()
+
 def auth_func():
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -27,7 +31,7 @@ def auth_func():
 
 
 def entity_report():
-  print("Creating Entity Report")
+  print(f"Creating {provider} Entity Report")
   token = auth_func()
   config = configparser.ConfigParser()
   config.read('config.ini')
@@ -37,43 +41,51 @@ def entity_report():
       'x-redlock-auth': token
   }
   payload = {}  
-  file_name = "undefended_resources.xlsx"
-  url = f'{base_url}/api/v33.01/cloud/discovery/entities?defended=false&offset=0'
+  file_name = f"undefended_resources_{today}_{provider}.xlsx"
+  url = f'{base_url}/api/v33.01/cloud/discovery/entities?defended=false&offset=0&provider={provider}'
   response = requests.request("GET", url, headers=headers, data=payload)
   print(f"Initial response Code: {response.status_code}")
-  data = json.loads(response.text)
+  try:
+    data = json.loads(response.text)
+  except json.JSONDecodeError:
+     print(f"No {provider} entities.")
+     return
   wb = openpyxl.Workbook()
   ws = wb.active
-  ws.title = "Undefended Resources"
-  wb_headers = ["Name", "ARN", "AccountID", "Region"]
+  ws.title = "Undefended Entities"
+  wb_headers = ["Name", "ARN", "AccountID", "Region", "Workload Type"]
   ws.append(wb_headers)
   offset = 0
+  if data is None:
+     print(f"No {provider} entities.")
+     return
+  print(f"Creating {file_name}")
   for item in data:
-    ws.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
+    ws.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', ''), item.get('serviceType', '')])
   
   while data is not None:
     offset += 50
-    url = f'{base_url}/api/v33.01/cloud/discovery/entities?defended=false&offset={offset}'
+    url = f'{base_url}/api/v33.01/cloud/discovery/entities?defended=false&offset={offset}&provider={provider}'
     response = requests.request("GET", url, headers=headers, data=payload)
     try:
       data = json.loads(response.text)
     except json.JSONDecodeError:
        wb.save(file_name)
-       print("Entity Run Complete")
+       print(f"{provider} Entity Run Complete")
        return
     if data is None:
       wb.save(file_name)
-      print("Entity Run Complete")
+      print(f"{provider} Entity Run Complete")
       return
     for item in data:
-      ws.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
+      ws.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', ''), item.get('serviceType', '')])
     wb.save(file_name)
-    print(f"Saved up to {offset}. Response Code: {response.status_code}")
+    print(f"Saved up to {offset}. Response Code: {response.status_code} Provider: {provider}")
     if offset % 1000 == 0: 
       token = auth_func()
 
 def vm_report():
-  print("Creating VM Report")
+  print(f"Creating {provider} VM Report")
   token = auth_func()
   config = configparser.ConfigParser()
   config.read('config.ini')
@@ -83,40 +95,55 @@ def vm_report():
       'x-redlock-auth': token
   }
   payload = {}  
-  file_name = "undefended_resources.xlsx"
-  url = f'{base_url}/api/v33.01/cloud/discovery/vms?offset=0&hasDefender=false'
+  file_name = f"undefended_resources_{today}_{provider}.xlsx"
+  url = f'{base_url}/api/v33.01/cloud/discovery/vms?offset=0&hasDefender=false&provider={provider}'
   response = requests.request("GET", url, headers=headers, data=payload)
   print(f"Initial response Code: {response.status_code}")
-  data = json.loads(response.text)
-  wb = load_workbook(file_name)
-  ws2 = wb.create_sheet(title="VMs")
-  wb_headers = ["Name", "ARN", "AccountID", "Region"]
+  try:
+    data = json.loads(response.text)
+  except json.JSONDecodeError:
+     print(f"No {provider} VMs.")
+     return
+  try:
+    wb = load_workbook(file_name)
+  except FileNotFoundError:
+     wb = openpyxl.Workbook()
+  ws2 = wb.create_sheet(title="Undefended VMs")
+  wb_headers = ["Name", "ARN", "AccountID", "Region", "Hostname"]
   ws2.append(wb_headers)
   offset = 0
+  if data is None:
+    print(f"No {provider} VMs.")
+    return
+  print(f"Creating {file_name}")
   for item in data:
-    ws2.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
+    ws2.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', ''), item.get('hostname', '')])
   
   while data is not None:
     offset += 50
-    url = f'{base_url}/api/v33.01/cloud/discovery/vms?offset={offset}&hasDefender=false'
+    url = f'{base_url}/api/v33.01/cloud/discovery/vms?offset={offset}&hasDefender=false&provider={provider}'
     response = requests.request("GET", url, headers=headers, data=payload)
     try:
       data = json.loads(response.text)
     except json.JSONDecodeError:
        wb.save(file_name)
-       print("VM Run Complete")
+       print(f"{provider} VM Run Complete")
        return
     if data is None:
       wb.save(file_name)
-      print("VM Run Complete")
+      print(f"{provider} VM Run Complete")
       return
     for item in data:
-      ws2.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', '')])
+      ws2.append([item.get('name', ''), item.get('arn', ''), item.get('accountID', ''), item.get('region', ''), item.get('hostname', '')])
     wb.save(file_name)
-    print(f"Saved up to {offset}. Response Code: {response.status_code}")
+    print(f"Saved up to {offset}. Response Code: {response.status_code} Provider: {provider}")
     if offset % 1000 == 0: 
       token = auth_func()
-  
-  
-entity_report()
-vm_report()
+
+
+
+providers = ["others", "alibaba", "oci", "gcp", "azure", "aws" ]
+for provider in providers:
+  entity_report()
+  vm_report()
+
